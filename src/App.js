e2099c3efc815af0e317c6929fe5de9158733f7d
Shane <shanemcneil19@gmail.com>
2019-09-14 13:31:07 -0700

    create Summary component

--- src/App.js
+++ src/App.js
@@ -76,25 +76,9 @@ class App extends Component {
       );
     });
 
-    const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const selectedOption = this.state.selected[feature];
+    
 
-      return (
-        <div className="summary__option" key={featureHash}>
-          <div className="summary__option__label">{feature} </div>
-          <div className="summary__option__value">{selectedOption.name}</div>
-          <div className="summary__option__cost">
-            {USCurrencyFormat.format(selectedOption.cost)}
-          </div>
-        </div>
-      );
-    });
-
-    const total = Object.keys(this.state.selected).reduce(
-      (acc, curr) => acc + this.state.selected[curr].cost,
-      0
-    );
+    
 
     return (
       <div className="App">
@@ -104,8 +88,8 @@ class App extends Component {
         <main>
           <CustomizeForm features={features}/>
           <Cart 
-            summary={summary}
-            total={total}
+            
+            selected={this.state.selected}
             USCurrencyFormat={USCurrencyFormat} />
         </main>
       </div>
--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import './Cart.css';
+import Summary from '../Summary/Summary';
 
 export default class Cart extends Component {
+    
+
     render() {
+        const total = Object.keys(this.props.selected).reduce(
+            (acc, curr) => acc + this.props.selected[curr].cost,
+            0
+          );
+
         return (
             <section className="main__summary">
                 <h2>Your cart</h2>
-                {this.props.summary}
+                <Summary 
+                    USCurrencyFormat={this.props.USCurrencyFormat} 
+                    selected={this.props.selected}/>
                 <div className="summary__total">
                     <div className="summary__total__label">Total</div>
                     <div className="summary__total__value">
-                        {this.props.USCurrencyFormat.format(this.props.total)}
+                        {this.props.USCurrencyFormat.format(total)}
                     </div>
                 </div>
             </section>
