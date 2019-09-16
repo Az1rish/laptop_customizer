9a56f220feafac4ee1772b3c38274a50a1301f85
Shane <shanemcneil19@gmail.com>
2019-09-16 10:01:21 -0700

    add key to Options component

--- src/Features/Features.js
+++ src/Features/Features.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import './Features.css';
 import Options from '../Options/Options';
+// Normalizes string as a slug - a string that is safe to use
+// in both URLs and html attributes
+import slugify from 'slugify';
 
 export default class Features extends Component {
     render() {
         const features = Object.keys(this.props.features).map((feature, idx) => {
             const featureHash = feature + '-' + idx;
+            
             const options = this.props.features[feature].map(item => {
+                const itemHash = slugify(JSON.stringify(item));
                 return (
                     <Options 
                         item={item}
+                        key={itemHash}
                         feature={feature}
                         selected={this.props.selected}
                         USCurrencyFormat={this.props.USCurrencyFormat}
